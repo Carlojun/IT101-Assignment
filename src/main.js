fd7bfcd918944437eba82d25812ed15ae9ec28d2
CarlojunPesical <123234453+Carlojun@users.noreply.github.com>
2025-08-20 14:28:33 +0800

    v.1.4

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import './style.css'
-
 const container = document.querySelector('.container');
 const registerBtn = document.querySelector('.register-btn');
 const loginBtn = document.querySelector('.login-btn');
